5d9da26df470ed711630bc1069d07af3d5d30571
benediktpetur <benedikt.petur@gmail.com>
2023-11-24 22:57:10 +0000

    bætt við scripti

--- readme.md
+++ readme.md
@@ -12,5 +12,13 @@ stylelint
 sass
 dev
 
+Forsíða og Flokkar settir upp
+Nýjar vörur í vinnslu
+
+Leita gluggi undir Flokkar(category.html)
+
 Sett upp þannig að síðurnar eru leitanlegar
-sett upp þannig categorys eru leitanlegar
+sett upp þannig categorys eru leitanlegar (Ekki enn komið upp category síða Fallið á tíma)
+
+Vörur settar upp, 12 vörur fyrir 1 flokk komið (Féll á tíma... 12 x 12 = 144 vörusíður? ákveðið að setja eh virkni frekar)
+
--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const files = [
     "hattur.html", "hlaupavesti.html", "hufa.html", "jakki.html",
     "ledurjakki.html", "naerbuxur.html", "nike.html", "peysa.html",
     "skirta.html", "skor.html", "sokkar.html", "ull.html"
-].map(file => 'sidur/vorur/${file}');
+].map(file => '/sidur/vorur/${file}');
 
 document.getElementById('searchInput').addEventListener('input', function(e) {
     const searchQuery = e.target.value.toLowerCase();
@@ -51,3 +51,46 @@ document.getElementById('searchInput').addEventListener('input', function(e) {
         resultsContainer.appendChild(link);
     });
 });
+document.getElementById('searchInput').addEventListener('input', function(e) {
+    var searchQuery = e.target.value.toLowerCase(); 
+    searchWebsite(searchQuery); 
+});
+
+var websiteContent = [
+    { title: "index", content: "heimasíða", url: "index.html" },
+    { title: "category", content: "Leita", url: "/sidur/category.html" },
+    { title: "hattur", content: "Hattur", url: "/sidur/vorur/hattur.html" },
+    { title: "Hlaupavesti", content: "Hlaupavesti", url: "/sidur/vorur/halupavesti.html" },
+    { title: "Hufa", content: "hufa", url: "/sidur/vorur/hufa.html" },
+    { title: "Jakki", content: "jakki", url: "/sidur/vorur/jakki.html" },
+    { title: "Leðurjakki", content: "ledurjakki", url: "/sidur/vorur/ledurjakki.html" },
+    { title: "Nærbuxur", content: "naerbuxur", url: "/sidur/vorur/naerbuxur.html" },
+    { title: "Nike galli", content: "nike galli", url: "/sidur/vorur/nike.html" },
+    { title: "Peysa", content: "peysa", url: "/sidur/vorur/peysa.html" },
+    { title: "Skirta", content: "skirta", url: "/sidur/vorur/skirta.html" },
+    { title: "Skór", content: "skor", url: "/sidur/vorur/skor.html" },
+    { title: "Sokkar", content: "sokkar", url: "/sidur/vorur/sokkar.html" },
+    { title: "Ullarpeysa", content: "ullarpeysa", url: "/sidur/vorur/ull.html" },
+
+   
+];
+
+function searchWebsite(query) {
+    var results = [];
+    if (query.length > 0) {
+        results = websiteContent.filter(function(page) {
+            return page.content.toLowerCase().includes(query) || page.title.toLowerCase().includes(query);
+        });
+    }
+    displayResults(results);
+}
+function displayResults(results) {
+    var resultsContainer = document.getElementById('searchResults');
+    resultsContainer.innerHTML = ''; 
+
+    results.forEach(function(page) {
+        var resultItem = document.createElement('div');
+        resultItem.innerHTML = `<a href="${page.url}">${page.title}</a>`;
+        resultsContainer.appendChild(resultItem);
+    });
+}
