97bd03a2d38174a72b1b8db3e92e91ddbc67c55b
benediktpetur <benedikt.petur@gmail.com>
2023-11-24 17:59:17 +0000

    uppsettning á myndum og 12 vörum
    vinnsla

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 
 window.onload = function() {
     var queryString = window.location.search;
-
     var urlParams = new URLSearchParams(queryString);
-
     var category = urlParams.get('category');
 
-   console.log("Category selected: " + category);
+    console.log("Category selected: " + category);
+
+    
+    document.getElementById('searchForm').onsubmit = function() {
+        
+        var searchQuery = document.getElementById('searchInput').value;
+        console.log("Search Query: " + searchQuery);
+
+
+        return false; 
+    };
 };
